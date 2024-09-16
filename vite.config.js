e48c22ebb7dfb8f53f89b01017196f878024cb75
HammadAhmad221 <128469279+HammadAhmad221@users.noreply.github.com>
2024-09-16 19:22:31 +0500

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,24 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    // historyApiFallback: true,
-    appType: 'mpa',
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  // Ensures the server will fallback to index.html for unknown routes
-});
+})
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     historyApiFallback: true,
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+// });
